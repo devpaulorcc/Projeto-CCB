23ce23f6229d130c02dab423606bce0d7036a34b
Dev-AlexandreS <alegamerth@gmail.com>
2024-05-27 21:31:41 -0300

    📦 UPDATE: Reorganização de estrutura CSS

--- CCB-SYSTEM/app/Views/cad-first.php
+++ CCB-SYSTEM/app/Views/cad-first.php
@@ -24,29 +24,39 @@
                 <label for="rg">RG</label>
                 <input type="text" name="rg" id="rg" placeholder="Digite o RG">
                 
-                <label for="rg">ENDEREÇO</label>
-                <input type="text" name="endereco" id="endereco" placeholder="Digite o endereço">
                 
-                <label for="rg">COMPLEMENTO</label>
-                <input type="text" name="comp" id="comp" placeholder="Digite o complemento">
-            
                 <label for="dataNasc">DATA DE NASCIMENTO</label>
                 <input type="date" name="dataNasc" id="dataNasc">
+                
+                <div class="container-num-cep">
+                    <div class="container-num-cep-item">
+                        <label for="cep">CEP</label>
+                        <input type="number" name="cep" id="cep">
+                    </div>
+                    <div class="container-num-cep-item">
+                        <label for="numCasa">NÚMERO</label>
+                        <input type="number" name="numCasa" id="numCasa">
+                    </div>
+                </div>
+                
+                <label for="cidade">CIDADE</label>
+                <input type="text" name="comp" id="cidade" placeholder="Digite a cidade">
             </div>
             <div class="container-inputs">
                 
                 <label for="nome">NOME VOLUNTÁRIO</label>
                 <input type="text" name="nome" id="nome" placeholder="Digite o nome">
                 
-                <label for="bairro">BAIRRO</label>
-                <input type="text" name="bairro" id="bairro" placeholder="Digite o bairro">
-                
                 <label for="ctt-fixo">CONTATO FIXO</label>
                 <input type="tel" name="ctt-fixo" id="ctt-fixo" placeholder="Digite o telefone fixo">
                 
+                
+                <label for="rua">Rua</label>
+                <input type="text" name="endereco" id="rua" placeholder="Digite o nome da rua">
+                
                 <label for="congregacao">NOME DA CONGREGAÇÃO</label>
                 <input type="text" name="congregacao" id="congregacao" placeholder="Digite o nome da congregação">
-            
+                
             </div>
             <div class="container-inputs">
                 <label for="gruposDeTrabalho">GRUPO DE TRABALHO</label>
@@ -56,20 +66,13 @@
                     <option >#</option>
                     <option >#</option>
                 </select>
-
-                <div class="container-num-cep">
-                    <div class="container-num-cep-item">
-                        <label for="numCasa">NÚMERO</label>
-                        <input type="number" name="numCasa" id="numCasa">
-                    </div>
-                    <div class="container-num-cep-item">
-                        <label for="cep">CEP</label>
-                        <input type="number" name="cep" id="cep">
-                    </div>
-                </div>
+                
                 
                 <label for="ctt-celular">CONTATO CELULAR</label>
                 <input type="tel" name="ctt-celular" id="ctt-celular">
+                
+                <label for="bairro">BAIRRO</label>
+                <input type="text" name="bairro" id="bairro" placeholder="Digite o bairro">
 
                 <label for="ftperfil">ADICIONE UMA FOTO DE PERFIL</label>
                 <label class="upload" for="ftperfil">UPLOAD</label>
